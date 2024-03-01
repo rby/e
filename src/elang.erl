3e72340717d6ffbdcf0092ae1b17482746b604c3
r5 <ramzi.benyahia@gmail.com>
2024-03-01 23:54:21 +0000

    [parser] Long commit for parsing numbers etc.

--- src/elang.erl
+++ src/elang.erl
@@ -7,6 +7,7 @@
 %% TODO remove lexemes or actually moved completely outside of this file
 -export([main/1, lexemes/1]).
 
+-define(Spaces, #{$\t => 1, $\s => 1}).
 -define(seps, [$\t, $;, $\s]).
 %%====================================================================
 %% API functions
@@ -89,10 +90,8 @@ scan_tokens(Tokens, Line) ->
     ).
 
 process(Atom) when is_atom(Atom) -> Atom;
-process(';') ->
-    ';';
-process(":=") ->
-    ':=';
+process(F) when is_float(F) -> {float64, F};
+process(I) when is_integer(I) -> {integer, I};
 process("_") ->
     '_';
 process(X) ->
@@ -136,13 +135,73 @@ lexemes("", Acc, EndPosition) ->
     {ok, Acc, EndPosition};
 lexemes([$; | Rest], Acc, Col) ->
     lexemes(Rest, [{';', Col} | Acc], Col + 1);
+lexemes([$. | Rest], Acc, Col) ->
+    lexemes(Rest, [{'.', Col} | Acc], Col + 1);
 lexemes([$& | Rest], Acc, Col) ->
     lexemes(Rest, [{'&', Col} | Acc], Col + 1);
-lexemes([$: | [$= | Rest]], Acc, Col) ->
+lexemes(":=" ++ Rest, Acc, Col) ->
     lexemes(Rest, [{':=', Col} | Acc], Col + 2);
 lexemes([$: | Rest], Acc, Col) ->
     lexemes(Rest, [{':', Col} | Acc], Col + 1);
-lexemes([S | Rest], Acc, Col) when (S == $\s) or (S == $\t) ->
+lexemes(String = [$+, D | _], Acc, Col) when D >= $0, D =< $9 ->
+    {ok, Number, Col2, Rest} = lexemes_number(String, Col),
+    lexemes(Rest, [{Number, Col} | Acc], Col2);
+lexemes(String = [$-, D | _], Acc, Col) when D >= $0, D =< $9 ->
+    {ok, Number, Col2, Rest} = lexemes_number(String, Col),
+    lexemes(Rest, [{Number, Col} | Acc], Col2);
+lexemes(String = [D | _], Acc, Col) when D >= $0, D =< $9 ->
+    {ok, Number, Col2, Rest} = lexemes_number(String, Col),
+    lexemes(Rest, [{Number, Col} | Acc], Col2);
+lexemes([$+ | Rest], Acc, Col) ->
+    lexemes(Rest, [{'+', Col} | Acc], Col + 1);
+lexemes([$? | Rest], Acc, Col) ->
+    lexemes(Rest, [{'?', Col} | Acc], Col + 1);
+lexemes([$[ | Rest], Acc, Col) ->
+    lexemes(Rest, [{'[', Col} | Acc], Col + 1);
+lexemes([$] | Rest], Acc, Col) ->
+    lexemes(Rest, [{']', Col} | Acc], Col + 1);
+lexemes([${ | Rest], Acc, Col) ->
+    lexemes(Rest, [{'{', Col} | Acc], Col + 1);
+lexemes([$} | Rest], Acc, Col) ->
+    lexemes(Rest, [{'}', Col} | Acc], Col + 1);
+lexemes([$( | Rest], Acc, Col) ->
+    lexemes(Rest, [{'(', Col} | Acc], Col + 1);
+lexemes([$) | Rest], Acc, Col) ->
+    lexemes(Rest, [{')', Col} | Acc], Col + 1);
+lexemes([$, | Rest], Acc, Col) ->
+    lexemes(Rest, [{',', Col} | Acc], Col + 1);
+lexemes("<-" ++ Rest, Acc, Col) ->
+    lexemes(Rest, [{'<-', Col} | Acc], Col + 3);
+lexemes("def" ++ [S | Rest], Acc, Col) when is_map_key(S, ?Spaces) ->
+    lexemes(Rest, [{'def', Col} | Acc], Col + 4);
+lexemes("var" ++ [S | Rest], Acc, Col) when is_map_key(S, ?Spaces) ->
+    lexemes(Rest, [{'var', Col} | Acc], Col + 4);
+lexemes("try" ++ [S | Rest], Acc, Col) when is_map_key(S, ?Spaces) ->
+    lexemes(Rest, [{'try', Col} | Acc], Col + 4);
+lexemes("if" ++ [S | Rest], Acc, Col) when is_map_key(S, ?Spaces) ->
+    lexemes(Rest, [{'if', Col} | Acc], Col + 3);
+lexemes("else" ++ [S | Rest], Acc, Col) when is_map_key(S, ?Spaces) ->
+    lexemes(Rest, [{'else', Col} | Acc], Col + 5);
+lexemes("escape" ++ [S | Rest], Acc, Col) when is_map_key(S, ?Spaces) ->
+    lexemes(Rest, [{'escape', Col} | Acc], Col + 7);
+lexemes("catch" ++ [S | Rest], Acc, Col) when is_map_key(S, ?Spaces) ->
+    lexemes(Rest, [{'catch', Col} | Acc], Col + 6);
+lexemes("finally" ++ [S | Rest], Acc, Col) when is_map_key(S, ?Spaces) ->
+    lexemes(Rest, [{'finally', Col} | Acc], Col + 8);
+lexemes("method" ++ [S | Rest], Acc, Col) when is_map_key(S, ?Spaces) ->
+    lexemes(Rest, [{'method', Col} | Acc], Col + 7);
+lexemes("match" ++ [S | Rest], Acc, Col) when is_map_key(S, ?Spaces) ->
+    lexemes(Rest, [{'match', Col} | Acc], Col + 6);
+lexemes("implements" ++ [S | Rest], Acc, Col) when is_map_key(S, ?Spaces) ->
+    lexemes(Rest, [{'implements', Col} | Acc], Col + 11);
+lexemes("/**" ++ Rest, Acc, Col) ->
+    lexemes(Rest, [{'/**', Col} | Acc], Col + 3);
+lexemes("*/" ++ Rest, Acc, Col) ->
+    lexemes(Rest, [{'*/', Col} | Acc], Col + 2);
+lexemes(String = [D | _], Acc, Col) when D >= $0, D =< $9 ->
+    {ok, Number, Rest, Col2} = lexemes_number(String, Col),
+    lexemes(Rest, [{Number, Col} | Acc], Col2);
+lexemes([S | Rest], Acc, Col) when is_map_key(S, ?Spaces) ->
     lexemes(Rest, Acc, Col + 1);
 lexemes(String, Acc, Col) ->
     %% rollout all this to the most boring state
@@ -165,27 +224,53 @@ lexemes(String, Acc, Col) ->
             lexemes(Remain, Acc, NewCol)
     end.
 
+lexemes_number([$+ | Rest], Col) ->
+    lexemes_number(Rest, Col + 1, []);
+lexemes_number([$- | Rest], Col) ->
+    Res = lexemes_number(Rest, Col + 1, []),
+    io:format("Res = ~p~n", [Res]),
+    {ok, Num, Col2, String} = Res,
+    {ok, -Num, Col2, String};
+lexemes_number([D | Rest], Col) when D >= $0, D =< $9 ->
+    lexemes_number(Rest, Col + 1, [D]).
+lexemes_number([D | Rest], Col, NumStr) when D >= $0, D =< $9 ->
+    lexemes_number(Rest, Col + 1, [D | NumStr]);
+lexemes_number([$. | Rest], Col, NumStr) ->
+    lexemes_float(Rest, Col + 1, [$. | NumStr]);
+lexemes_number(String, Col, NumStr) ->
+    {ok, list_to_integer(lists:reverse(NumStr)), Col, String}.
+lexemes_float([D | Rest], Col, NumStr) when D >= $0, D =< $9 ->
+    lexemes_float(Rest, Col + 1, [D | NumStr]);
+lexemes_float(String, Col, NumStr) ->
+    {ok, list_to_float(lists:reverse(NumStr)), Col, String}.
+
 -ifdef(TEST).
 
+lexemes_number_test_() ->
+    [
+        ?_assertMatch({ok, -23.34, 1 + 6, " def"}, lexemes_number("-23.34 def", 1)),
+        ?_assertMatch({ok, 2334, 1 + 5, " def"}, lexemes_number("+2334 def", 1)),
+        ?_assertMatch({ok, 4, 1, ";"}, lexemes_number("4;", 0))
+    ].
 lexemes_test_() ->
     [
         ?_assertMatch(
             {ok,
                 [
-                    {"def", 0},
+                    {'def', 0},
                     {"x", 4},
                     {':=', 6},
-                    {"3", 9},
+                    {3, 9},
                     {';', 10},
                     {"y", 12},
                     {':=', 14},
-                    {"4", 17}
+                    {4, 17}
                 ],
                 18},
             lexemes("def x := 3; y := 4", true)
         ),
         ?_assertMatch(
-            {ok, [{'&', 0}, {"x", 1}, {':=', 3}, {"4", 6}, {';', 7}], 8},
+            {ok, [{'&', 0}, {"x", 1}, {':=', 3}, {4, 6}, {';', 7}], 8},
             lexemes("&x := 4;", true)
         )
         %% ideally this should work too
